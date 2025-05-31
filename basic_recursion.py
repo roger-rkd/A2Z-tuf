9eb2f96d0c1802f1755ee29865c2f834186fdcb8
dubey <dubeysmail@gmail.com>
2025-05-31 15:38:58 +0100

    re 2 : print name n times

--- basic_recursion.py
+++ basic_recursion.py
@@ -4,4 +4,12 @@ def print_n_numbers(n):
         n+=1
         print_n_numbers(n) # recursive function
 
-print_n_numbers(0)
+# print_n_numbers(0)
+
+def print_name_n_times(n):
+    if n<=5:
+        print("Mahadev")
+        n+=1
+        print_name_n_times(n) # recursive function
+
+print_name_n_times(1)
